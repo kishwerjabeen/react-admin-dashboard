33068a953d2b2d40d7ddc7facece28a30ac9ee7e
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-12-10 01:13:26 +0500

    navbar set

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./home.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
   return (
     <div className='home'>
 
-    <Sidebar/>
+      <Sidebar />
 
-    <div className='homeContainer'>Container</div>
-    
-    
+      <div className='homeContainer'>
+        <Navbar />
+        home Container
+      </div>
+
+
     </div>
   )
 }
