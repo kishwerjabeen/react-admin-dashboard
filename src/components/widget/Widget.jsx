019134c29ff4a4ab686951b8ab18332a21496d38
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-12-15 00:12:29 +0500

    widget css complate

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import "./widget.scss"
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
 const Widget = () => {
     return (
         <div className='widget'>
-            <div className='Left'>
-                Left
+            <div className='left'>
+                <span className='title'>USERS</span>
+                <span className='counter'>123456</span>
+                <span className='link'>See All user</span>
             </div>
             <div className='right'>
-                Right
+                <div className='percentage positive'>
+                    <KeyboardArrowUpIcon />
+                    20%
+                </div>
+                <PersonOutlinedIcon className='icon'/>
             </div>
         </div>
     )
