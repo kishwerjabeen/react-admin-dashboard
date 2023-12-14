32423ebf1af068981da27bff7434fb6f3bfee73f
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-12-14 22:52:01 +0500

    navbar working

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,38 +18,46 @@ const Navbar = () => {
         {/* search */}
         <div className='search'>
           <input type='text' placeholder='Search....' />
-          <SearchOutlinedIcon />
+          <SearchOutlinedIcon className="icon"  />
         </div>
 
         {/* items */}
 
         <div className='items'>
           <div className='item'>
-            <LanguageOutlinedIcon />
+            <LanguageOutlinedIcon  className="icon" />
             English
           </div>
 
           <div className='item'>
-            <DarkModeOutlinedIcon />
+            <DarkModeOutlinedIcon className="icon"  />
           </div>
 
           <div className='item'>
-            <FullscreenExitOutlinedIcon />
+            <FullscreenExitOutlinedIcon className="icon"  />
+            <div className='counter'>1</div>
+          </div>
+
+          <div className='item'> 
+            <NotificationsNoneOutlinedIcon  className="icon" />
+            <div className='counter'>2</div>
           </div>
 
           <div className='item'>
-            <NotificationsNoneOutlinedIcon />
-          </div>
-
-          <div className='item'>
-            <ChatBubbleOutlineOutlinedIcon />
+            <ChatBubbleOutlineOutlinedIcon className="icon"  />
           </div>
 
 
           <div className='item'>
-            <ListOutlinedIcon />
+            <ListOutlinedIcon className="icon"  />
           </div>
-
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div> 
 
 
 
